21d1952814346e95e7b92622a4a464d2caee406f
Abdulboriy <abdulborimahamatjanov86@gmail.com>
2025-04-13 14:04:19 +0600

    the exam-project has been successfully started

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Exam project')
+    .setDescription('The exam-projct REST API description')
+    .setVersion('1.0')
+    .addSecurityRequirements('bearer', ['bearer'])
+    .addBearerAuth()
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  await app.listen(process.env.PORT ?? 3000, () =>
+    console.log(`Server started on port: ${process.env.PORT}`),
+  );
+}
+
+bootstrap();
