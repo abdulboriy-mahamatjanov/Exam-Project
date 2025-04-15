df5ab7a812092db1b23884641b7415d576405368
Abdulboriy <abdulborimahamatjanov86@gmail.com>
2025-04-16 02:37:20 +0600

    all table modules added to app.module file

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,12 @@ import { UserModule } from './user/user.module';
 import { EskizService } from './eskiz/eskiz.service';
 import { EskizModule } from './eskiz/eskiz.module';
 import { ShowCasesModule } from './show-cases/show-cases.module';
+import { PartrensModule } from './partrens/partrens.module';
+import { ContactsModule } from './contacts/contacts.module';
+import { FrequentlyAskedQuestionsModule } from './frequently_asked_questions/frequently_asked_questions.module';
+import { AboutPageModule } from './about-page/about-page.module';
+import { DataCompanyModule } from './data-company/data-company.module';
+import { LevelsModule } from './levels/levels.module';
 
 @Module({
   imports: [
@@ -29,6 +35,12 @@ import { ShowCasesModule } from './show-cases/show-cases.module';
     UserModule,
     EskizModule,
     ShowCasesModule,
+    PartrensModule,
+    ContactsModule,
+    FrequentlyAskedQuestionsModule,
+    AboutPageModule,
+    DataCompanyModule,
+    LevelsModule,
   ],
   controllers: [],
   providers: [],
