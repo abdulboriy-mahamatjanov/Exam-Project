477b930b97f66d3232f7cb21c62749dce436cc01
Abdulboriy <abdulborimahamatjanov86@gmail.com>
2025-04-18 02:50:50 +0600

    users file has updated

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,20 @@
 import {
   Controller,
   Get,
-  Post,
   Body,
   Patch,
   Param,
   Delete,
+  UseGuards,
+  Query,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UpdateAuthDto } from 'src/auth/dto/update-auth.dto';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiQuery, ApiTags } from '@nestjs/swagger';
+import { AuthGuard } from 'src/guards/jwt_token.guard';
+import { Roles } from 'src/guards/role.decorator';
+import { UserRoles } from 'src/auth/dto/register-user.dto';
+import { RoleGuard } from 'src/guards/rolePolice.guard';
 
 @ApiTags('Users')
 @Controller('user')
@@ -17,24 +22,45 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @ApiOperation({ summary: 'Get all Users' })
+  @ApiQuery({ name: 'page', required: false, type: Number })
+  @ApiQuery({ name: 'limit', required: false, type: Number })
+  @ApiQuery({
+    name: 'order',
+    required: false,
+    type: String,
+    enum: ['asc', 'desc'],
+  })
+  @ApiQuery({ name: 'search', required: false, type: String })
+  // @Roles(UserRoles.ADMIN)
+  // @UseGuards(RoleGuard)
+  // @UseGuards(AuthGuard)
   @Get()
-  findAll() {
-    return this.userService.findAll();
+  findAll(@Query() query: any) {
+    return this.userService.findAll(query);
   }
 
   @ApiOperation({ summary: 'Get one User By ID' })
+  @Roles(UserRoles.ADMIN)
+  @UseGuards(RoleGuard)
+  @UseGuards(AuthGuard)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.userService.findOne(id);
   }
 
   @ApiOperation({ summary: 'Update User By ID' })
+  // @Roles(UserRoles.ADMIN)
+  // @UseGuards(RoleGuard)
+  // @UseGuards(AuthGuard)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateAuthDto) {
     return this.userService.update(id, updateUserDto);
   }
 
   @ApiOperation({ summary: 'Delete User By ID' })
+  // @Roles(UserRoles.ADMIN)
+  // @UseGuards(RoleGuard)
+  // @UseGuards(AuthGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.userService.remove(id);
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,20 +1,53 @@
 import {
   BadRequestException,
+  HttpException,
   Injectable,
   NotFoundException,
 } from '@nestjs/common';
-import { UserRole } from 'generated/prisma';
-import { CreateAuthDto } from 'src/auth/dto/create-auth.dto';
 import { UpdateAuthDto } from 'src/auth/dto/update-auth.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { CloudinaryService } from 'src/uploads/uploads.service';
 
 @Injectable()
 export class UserService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly cloudinaryService: CloudinaryService,
+  ) {}
 
-  async findAll() {
+  async findAll(query: any) {
     try {
-      const Users = await this.prisma.users.findMany();
+      const { page = 1, limit = 10, order = 'desc', search = '' } = query;
+
+      const Users = await this.prisma.users.findMany({
+        where: {
+          OR: [
+            {
+              fullName: {
+                contains: search,
+                mode: 'insensitive',
+              },
+            },
+            {
+              phone: {
+                contains: search,
+                mode: 'insensitive',
+              },
+            },
+          ],
+        },
+
+        include: {
+          region: true,
+        },
+
+        skip: (page - 1) * limit,
+        take: Number(limit),
+
+        orderBy: {
+          fullName: order,
+        },
+      });
       if (!Users.length) return { message: 'No Users found' };
 
       const data = {
@@ -30,7 +63,10 @@ export class UserService {
 
   async findOne(id: string) {
     try {
-      const User = await this.prisma.users.findFirst({ where: { id } });
+      const User = await this.prisma.users.findFirst({
+        where: { id },
+        include: { region: true },
+      });
       if (!User) throw new NotFoundException('User not found ❗');
 
       return { User };
@@ -42,13 +78,28 @@ export class UserService {
   async update(id: string, updateUserDto: UpdateAuthDto) {
     try {
       const user = await this.findOne(id);
+
       if (!user) throw new NotFoundException('User not found ❗');
 
       const checkRegion = await this.prisma.regions.findFirst({
         where: { id: updateUserDto.regionId },
       });
-
       if (!checkRegion) throw new NotFoundException('Region not found ❗');
+
+      let oldPublicId = this.cloudinaryService.getPublicId(user.User.avatar);
+      let newPublicId: string | null = null;
+
+      if (updateUserDto.avatar) {
+        newPublicId = this.cloudinaryService.getPublicId(updateUserDto.avatar);
+
+        const checkImage = await this.cloudinaryService.checkImage(newPublicId);
+        if (!checkImage)
+          throw new BadRequestException('Image is not available yet ❗');
+
+        if (oldPublicId !== newPublicId) {
+          await this.cloudinaryService.deleteImage(oldPublicId);
+        }
+      }
 
       const newUser = await this.prisma.users.update({
         where: { id },
@@ -57,6 +108,9 @@ export class UserService {
 
       return { newUser };
     } catch (error) {
+      if (error instanceof HttpException) {
+        throw error;
+      }
       throw new BadRequestException(error.message);
     }
   }
@@ -66,6 +120,12 @@ export class UserService {
       const user = await this.findOne(id);
       if (!user) throw new NotFoundException('User not found ❗');
 
+      let publicId = this.cloudinaryService.getPublicId(user.User.avatar);
+      let checkImage = await this.cloudinaryService.checkImage(publicId);
+
+      if (checkImage) {
+        await this.cloudinaryService.deleteImage(publicId);
+      }
 
       await this.prisma.users.delete({ where: { id } });
       return { message: 'User is successfully deleted ✅' };
