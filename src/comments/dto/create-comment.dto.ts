3de00f399af6982411811015f9d20d955fc3c929
Abdulboriy <abdulborimahamatjanov86@gmail.com>
2025-04-18 02:47:35 +0600

    comments table has finished

--- src/comments/dto/create-comment.dto.ts
+++ src/comments/dto/create-comment.dto.ts
@@ -1,9 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNumber, IsOptional, IsString } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class CreateCommentDto {
   @ApiProperty({ example: 'something' })
   @IsString()
   @IsOptional()
   message: string;
+
+  @ApiProperty({ example: '' })
+  @IsString()
+  @IsNotEmpty()
+  userId: string;
 }
